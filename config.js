a4832a9af51356a3a618dee866a5b859528174f9
mariadonato <mariadelcielo34@gmail.com>
2023-12-20 13:56:38 -0300

    v0.0.2

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
 //console.log(process.env)
 
-const MODO_PERSISTENCIA = 'MONGODB'      //'MEM' - 'FILE' - 'MONGODB'
 const PORT = process.env.PORT || 8080
-//const STRCNX = 'mongodb://127.0.0.1'
-const STRCNX = 'mongodb+srv://cielo:cielo2010@cluster0.fo3olik.mongodb.net/?retryWrites=true&w=majority'
-const BASE = 'ecommerce'
+
+const MODO_PERSISTENCIA = process.env.MODO_PERSISTENCIA || 'MEM'    
+const STRCNX = process.env.STRCNX || 'mongodb://127.0.0.1'
+const BASE = process.env.BASE || 'test'
 
 export default {
     MODO_PERSISTENCIA,            // MODO_PERSISTENCIA : MODO_PERSISTENCIA
